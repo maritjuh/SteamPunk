c2ae285f819957b6a216270d10cf65a2c4927b7d
maritjuh <marit.peperkamp@stud.kftg.ch>
2019-05-21 17:07:35 +0200

    Caesar Verschlüsselung mit Hintergrund und HTML done.

--- SteamPunk/public_html/js/myscript.js
+++ SteamPunk/public_html/js/myscript.js
@@ -1,23 +1,42 @@
+let bg;
 let img;
 let angle = 0;
 let count = 0;
 
+function checkPin(){
+    var intput01 = document.getElementById('ipOne').value;
+    var intput02 = document.getElementById('ipTwo').value;
+    var intput03 = document.getElementById('ipThree').value;
+    var intput04 = document.getElementById('ipFour').value;
+    
+    if (input01 == 4 && input02 == 2 && input03 == 6 && input04 == 9){
+        console.log('Happy Brithday');
+    }else{
+        console.log('well...you dead');
+    }
+}
+
 function setup() {
-  createCanvas(400, 450);
+  bg = loadImage('img/BGiPad.png');
   img = loadImage('img/zahnrad_offical.png');
+  createCanvas(1366, 1024);
   angleMode(DEGREES);
 }
 
-function mouseReleased(){
-    if (mouseX < 400 && mouseY < 450 && count < 14){
-    background('img/hintergrund_5.jpg');
-    translate(200,225);
+function draw(){
+    background(bg);
+    translate(235, 300);
     rotate(angle);
-    
+    // https://p5js.org/reference/#/p5/pop
+    push();
     imageMode(CENTER);
-    image(img, 0, 0, img.width / 2, img.height / 2);
+    image(img, 0, 0, img.width / 2, img.height / 2);imageMode(CENTER);
+    pop();
+}
+
+function mouseReleased(){
+    if (mouseX < 400 && mouseY < 450 && count < 13){
     angle = angle + 12;  
-    
     count += 1;
     console.log(count);
     }
